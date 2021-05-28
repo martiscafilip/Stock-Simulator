5232df2ec168d40456733b57d1d799df24361c4b
Claudyou <59448503+Claudyou@users.noreply.github.com>
2021-05-28 11:28:38 +0300

    Update topApi.php

--- Stock-Simulator/app/api/topApi.php
+++ Stock-Simulator/app/api/topApi.php
@@ -5,7 +5,7 @@ require_once '../models/leaderboardModel.php';
 
 
 header("Content-Type: application/json");
-header("Accept-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: *");
 
 echo json_encode( getTop());
 
